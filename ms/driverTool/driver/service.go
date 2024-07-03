f8dd96199d7bae62e2121fb919d1f241e1792a2b
Admin <2762713521@qq.com>
2024-07-03 09:08:52 +0800

    fix mgr

--- ms/driverTool/driver/service.go
+++ ms/driverTool/driver/service.go
@@ -21,7 +21,7 @@ func SetUpService(serviceName string, driverFullPath string) {
 	connSCM := mylog.Check2(mgr.Connect())
 	service := CheckService(*connSCM, serviceName)
 	if service == nil {
-		service = mylog.Check2(CreateService(*connSCM, serviceName, driverFullPath))
+		service = mylog.Check2(CreateService(connSCM, serviceName, driverFullPath))
 	}
 	if !VerifyServiceConfig(service, driverFullPath) {
 		mylog.Check(connSCM.Disconnect())
@@ -36,20 +36,8 @@ func CheckService(connSCM mgr.Mgr, serviceName string) *mgr.Service {
 	return mylog.Check2(connSCM.OpenService(serviceName))
 }
 
-func CreateService(connSCM mgr.Mgr, serviceName string, driverPath string) (*mgr.Service, error) {
-	serviceConfig := mgr.Config{
-		ServiceType:  windows.SERVICE_KERNEL_DRIVER,
-		StartType:    windows.SERVICE_DEMAND_START,
-		ErrorControl: windows.SERVICE_ERROR_IGNORE,
-	}
-	service := mylog.Check2(CreateServiceImported(&connSCM, serviceName, driverPath, serviceConfig))
-
-	return service, nil
-}
-
 func VerifyServiceConfig(service *mgr.Service, driverPath string) bool {
 	serviceConfig := mylog.Check2(service.Config())
-
 	if serviceConfig.ServiceType != windows.SERVICE_KERNEL_DRIVER {
 		return false
 	}
@@ -64,9 +52,7 @@ func VerifyServiceConfig(service *mgr.Service, driverPath string) bool {
 
 func VerifyServiceRunning(serviceName string) error {
 	connSCM := mylog.Check2(mgr.Connect())
-
 	service := mylog.Check2(connSCM.OpenService(serviceName))
-
 	serviceStatus := mylog.Check2(service.Query())
 	if serviceStatus.State == windows.SERVICE_START_PENDING {
 		return errors.New(ErrServiceStartPending)
@@ -86,38 +72,43 @@ func RemoveService(serviceName string, driverFullPath string) {
 	mylog.Check(service.Delete())
 }
 
-func CreateServiceImported(m *mgr.Mgr, name, exepath string, c mgr.Config, args ...string) (*mgr.Service, error) {
+func CreateService(m *mgr.Mgr, serviceName, driverPath string, args ...string) (*mgr.Service, error) {
+	c := mgr.Config{
+		ServiceType:  windows.SERVICE_KERNEL_DRIVER,
+		StartType:    windows.SERVICE_DEMAND_START,
+		ErrorControl: windows.SERVICE_ERROR_IGNORE,
+	}
 	if c.StartType == 0 {
 		c.StartType = mgr.StartManual
 	}
 	if c.ServiceType == 0 {
 		c.ServiceType = windows.SERVICE_WIN32_OWN_PROCESS
 	}
-	h := mylog.Check2(windows.CreateService(m.Handle, toPtrImported(name), toPtrImported(c.DisplayName),
+	h := mylog.Check2(windows.CreateService(m.Handle, toPtr(serviceName), toPtr(c.DisplayName),
 		windows.SERVICE_ALL_ACCESS, c.ServiceType,
-		c.StartType, c.ErrorControl, toPtrImported(exepath), toPtrImported(c.LoadOrderGroup),
-		nil, toStringBlockImported(c.Dependencies), toPtrImported(c.ServiceStartName), toPtrImported(c.Password)))
+		c.StartType, c.ErrorControl, toPtr(driverPath), toPtr(c.LoadOrderGroup),
+		nil, toStringBlock(c.Dependencies), toPtr(c.ServiceStartName), toPtr(c.Password)))
 
 	if c.SidType != windows.SERVICE_SID_TYPE_NONE {
-		mylog.Check(updateSidTypeImported(h, c.SidType))
+		mylog.Check(updateSidType(h, c.SidType))
 	}
 	if c.Description != "" {
-		mylog.Check(updateDescriptionImported(h, c.Description))
+		mylog.Check(updateDescription(h, c.Description))
 	}
 	if c.DelayedAutoStart {
-		mylog.Check(updateStartUpImported(h, c.DelayedAutoStart))
+		mylog.Check(updateStartUp(h, c.DelayedAutoStart))
 	}
-	return &mgr.Service{Name: name, Handle: h}, nil
+	return &mgr.Service{Name: serviceName, Handle: h}, nil
 }
 
-func toPtrImported(s string) *uint16 {
+func toPtr(s string) *uint16 {
 	if len(s) == 0 {
 		return nil
 	}
 	return syscall.StringToUTF16Ptr(s)
 }
 
-func toStringBlockImported(ss []string) *uint16 {
+func toStringBlock(ss []string) *uint16 {
 	if len(ss) == 0 {
 		return nil
 	}
@@ -134,21 +125,20 @@ func toStringBlockImported(ss []string) *uint16 {
 	return &utf16.Encode([]rune(t))[0]
 }
 
-func updateSidTypeImported(handle windows.Handle, sidType uint32) error {
+func updateSidType(handle windows.Handle, sidType uint32) error {
 	return windows.ChangeServiceConfig2(handle, windows.SERVICE_CONFIG_SERVICE_SID_INFO, (*byte)(unsafe.Pointer(&sidType)))
 }
 
-func updateDescriptionImported(handle windows.Handle, desc string) error {
-	d := windows.SERVICE_DESCRIPTION{Description: toPtrImported(desc)}
+func updateDescription(handle windows.Handle, desc string) error {
+	d := windows.SERVICE_DESCRIPTION{Description: toPtr(desc)}
 	return windows.ChangeServiceConfig2(handle,
 		windows.SERVICE_CONFIG_DESCRIPTION, (*byte)(unsafe.Pointer(&d)))
 }
 
-func updateStartUpImported(handle windows.Handle, isDelayed bool) error {
+func updateStartUp(handle windows.Handle, isDelayed bool) error {
 	var d windows.SERVICE_DELAYED_AUTO_START_INFO
 	if isDelayed {
 		d.IsDelayedAutoStartUp = 1
 	}
-	return windows.ChangeServiceConfig2(handle,
-		windows.SERVICE_CONFIG_DELAYED_AUTO_START_INFO, (*byte)(unsafe.Pointer(&d)))
+	return windows.ChangeServiceConfig2(handle, windows.SERVICE_CONFIG_DELAYED_AUTO_START_INFO, (*byte)(unsafe.Pointer(&d)))
 }
