f24549f31c3fee908dfe851ecb692d6c0d18a2a5
Admin <2762713521@qq.com>
2024-07-04 20:11:33 +0800

    fix NewTab

--- widget/Tab.go
+++ widget/Tab.go
@@ -23,7 +23,6 @@ type TabClose struct {
 	unison.Panel
 	title       string
 	tooltip     string
-	closeable   bool
 	ScrollPanel *unison.ScrollPanel
 }
 
@@ -35,46 +34,40 @@ func (t *TabClose) SetTooltip(tooltip string) {
 	t.tooltip = tooltip
 }
 
-func (t *TabClose) SetCloseable(closeable bool) {
-	t.closeable = closeable
-}
-
 func (t *TabClose) SetScrollPanel(ScrollPanel *unison.ScrollPanel) {
 	t.ScrollPanel = ScrollPanel
 }
 
-func NewTabCloseWithTable[T any](table *Node[T], header *TableHeader[T], title string, tooltip string, closeable bool) *TabClose {
+func NewTabCloseWithTable[T any](table *Node[T], header *TableHeader[T], title string, tooltip string) *TabClose {
 	panel := NewPanel()
 	panel.AddChild(table)
 	panel.AddChild(header)
 
-	tabClose := NewTabClose(title, tooltip, closeable, panel)
+	tabClose := NewTabClose(title, tooltip, panel)
 	tabClose.ScrollPanel.SetColumnHeader(header)
 	return tabClose
 }
 
 type TabContent struct {
-	Title     string
-	Tooltip   string
-	Closeable bool
-	Panel     unison.Paneler
+	Title   string
+	Tooltip string
+	Panel   unison.Paneler
 }
 
 func NewTabCloses(tabContents ...TabContent) []*TabClose {
 	var tabCloses []*TabClose
 	for _, tabContent := range tabContents {
-		tabClose := NewTabClose(tabContent.Title, tabContent.Tooltip+" "+tabContent.Title, tabContent.Closeable, tabContent.Panel)
+		tabClose := NewTabClose(tabContent.Title, tabContent.Tooltip+" "+tabContent.Title, tabContent.Panel)
 		tabCloses = append(tabCloses, tabClose)
 	}
 	return tabCloses
 }
 
-func NewTabClose(title string, tooltip string, closeable bool, panel unison.Paneler) *TabClose {
+func NewTabClose(title string, tooltip string, panel unison.Paneler) *TabClose {
 	d := &TabClose{
 		Panel:       unison.Panel{},
 		title:       title,
 		tooltip:     tooltip,
-		closeable:   closeable,
 		ScrollPanel: nil,
 	}
 
@@ -90,7 +83,7 @@ func (t *TabClose) SetColumnHeader(header unison.Paneler) {
 }
 
 func (t *TabClose) MayAttemptClose() bool {
-	return t.closeable
+	return true
 }
 
 func (t *TabClose) AttemptClose() bool {
@@ -124,12 +117,11 @@ var _ unison.Dockable = &Tab{}
 
 type Tab struct {
 	unison.Panel
-	title     string
-	tooltip   string
-	closeable bool
+	title   string
+	tooltip string
 }
 
-func NewTabWithTable[T any](table *Node[T], header *TableHeader[T], title string, tooltip string, closeable bool) *Tab {
+func NewTabWithTable[T any](table *Node[T], header *TableHeader[T], title string, tooltip string) *Tab {
 	panel := NewPanel()
 	panel.AddChild(table)
 	panel.AddChild(header)
@@ -137,16 +129,15 @@ func NewTabWithTable[T any](table *Node[T], header *TableHeader[T], title string
 	scrollPanelFill := NewScrollPanelFill(panel)
 	scrollPanelFill.SetColumnHeader(header)
 
-	tab := NewTab(title, tooltip, closeable, scrollPanelFill)
+	tab := NewTab(title, tooltip, scrollPanelFill)
 	return tab
 }
 
-func NewTab(title string, tooltip string, closeable bool, panel unison.Paneler) *Tab {
+func NewTab(title string, tooltip string, panel unison.Paneler) *Tab {
 	d := &Tab{
-		Panel:     unison.Panel{},
-		title:     title,
-		tooltip:   tooltip,
-		closeable: closeable,
+		Panel:   unison.Panel{},
+		title:   title,
+		tooltip: tooltip,
 	}
 	d.Self = d
 	SetScrollLayout(d, 1)
@@ -157,7 +148,7 @@ func NewTab(title string, tooltip string, closeable bool, panel unison.Paneler) 
 func NewTabs(tabContents ...TabContent) []*Tab { // todo delete
 	var tabs []*Tab
 	for _, tabContent := range tabContents {
-		tab := NewTab(tabContent.Title, tabContent.Tooltip+" "+tabContent.Title, tabContent.Closeable, tabContent.Panel)
+		tab := NewTab(tabContent.Title, tabContent.Tooltip+" "+tabContent.Title, tabContent.Panel)
 		tabs = append(tabs, tab)
 	}
 	return tabs
@@ -169,10 +160,6 @@ func (t *Tab) SetTitle(title string) {
 
 func (t *Tab) SetTooltip(tooltip string) {
 	t.tooltip = tooltip
-}
-
-func (t *Tab) SetCloseable(closeable bool) {
-	t.closeable = closeable
 }
 
 func (t *Tab) TitleIcon(suggestedSize unison.Size) unison.Drawable {
