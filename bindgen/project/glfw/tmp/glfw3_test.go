71471341f7d74386154581c352725157534da397
ddkwork <2762713521@qq.com>
2024-07-07 19:22:20 +0800

    https://github.com/HyperDbg/gui/issues/167

--- bindgen/project/glfw/tmp/glfw3_test.go
+++ bindgen/project/glfw/tmp/glfw3_test.go
@@ -1,12 +1,8 @@
 package glfw
 
 import (
-	"syscall"
-	"testing"
-	"unsafe"
-
-	"github.com/ddkwork/golibrary/mylog"
 	"golang.org/x/sys/windows"
+	"testing"
 )
 
 func StringToBytePointer(s string) *byte {
@@ -15,7 +11,7 @@ func StringToBytePointer(s string) *byte {
 	return ptr
 }
 
-func TestInit(t *testing.T) {
+func TestBind(t *testing.T) {
 	windows.SetDllDirectory(".")
 	Init()
 	defer Terminate()
@@ -26,54 +22,6 @@ func TestInit(t *testing.T) {
 		SwapBuffers(w)
 		if WindowShouldClose(w) != 0 {
 			DestroyWindow(w)
-			break
-		}
-	}
-}
-
-func main() {
-	// Load the GLFW DLL
-	glfw := syscall.MustLoadDLL("glfw3.dll")
-
-	// Get the function pointers
-	glfwInit := glfw.MustFindProc("glfwInit")
-	glfwCreateWindow := glfw.MustFindProc("glfwCreateWindow")
-	glfwMakeContextCurrent := glfw.MustFindProc("glfwMakeContextCurrent")
-	glfwPollEvents := glfw.MustFindProc("glfwPollEvents")
-	glfwTerminate := glfw.MustFindProc("glfwTerminate")
-
-	// Initialize GLFW
-	ret, _ := mylog.Check3(glfwInit.Call())
-	if ret == 0 {
-		// fmt.Println("Failed to initialize GLFW:", err)
-		return
-	}
-	defer glfwTerminate.Call()
-
-	// Create a windowed mode window and its OpenGL context
-	width, height := 640, 480
-	title := "Hello World"
-	window, _ := mylog.Check3(glfwCreateWindow.Call(
-		uintptr(width), uintptr(height), uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))), 0, 0))
-	if window == 0 {
-		// fmt.Println("Failed to create GLFW window:", err)
-		return
-	}
-
-	// Make the window's context current
-	glfwMakeContextCurrent.Call(window)
-
-	// Main loop
-	for {
-		// Poll for and process events
-		glfwPollEvents.Call()
-
-		// Swap buffers
-		// (You would typically call your rendering code here)
-
-		// Check if the window should close
-		shouldClose, _, _ := syscall.Syscall(glfw.MustFindProc("glfwWindowShouldClose").Addr(), 1, window, 0, 0)
-		if shouldClose != 0 {
 			break
 		}
 	}
