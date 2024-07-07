e4bf6dbf2ec356fb8fb502b8380bd9e78614c3c5
ddkwork <2762713521@qq.com>
2024-07-07 19:26:07 +0800

    https://github.com/HyperDbg/gui/issues/167

--- bindgen/project/glfw/tmp/glfw3_test.go
+++ bindgen/project/glfw/tmp/glfw3_test.go
@@ -1,14 +1,18 @@
 package glfw3_test
 
 import (
-	glfw3 "github.com/ddkwork/app/bindgen/project/glfw/tmp"
-	"golang.org/x/sys/windows"
 	"testing"
+	"unsafe"
+
+	glfw3 "github.com/ddkwork/app/bindgen/project/glfw/tmp"
+	"github.com/ddkwork/golibrary/mylog"
+	"golang.org/x/sys/windows"
 )
 
 func TestBind(t *testing.T) {
 	windows.SetDllDirectory(".")
 	glfw3.Init()
+	mylog.Info("version", BytePointerToString(glfw3.GetVersionString()))
 	defer glfw3.Terminate()
 	w := glfw3.CreateWindow(200, 200, StringToBytePointer("hello word"), nil, nil)
 	glfw3.MakeContextCurrent(w)
@@ -26,4 +30,13 @@ func StringToBytePointer(s string) *byte {
 	bytes := []byte(s)
 	ptr := &bytes[0]
 	return ptr
+}
+
+func BytePointerToString(ptr *byte) string {
+	var bytes []byte
+	for *ptr != 0 {
+		bytes = append(bytes, *ptr)
+		ptr = (*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(ptr)) + 1))
+	}
+	return string(bytes)
 }
